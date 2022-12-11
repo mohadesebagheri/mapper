ee3c37acf890a410bb43175ade078020c98c31da
mohaddeseh.bagheri <mohaddeseh.bagheri@partsoftware.com>
2022-12-11 10:24:56 +0330

    added XMLconverter

--- xmlConverter.js
+++ xmlConverter.js
@@ -1,4 +1,20 @@
+const XMLParser = require('xml2json');
 class XmlConverter{
+
+    constructor(jsonConverter) {
+        this.jsonConverter = jsonConverter;
+    }
+
+    convert2json(apiResult){
+        const jsonResult = XMLParser.toJson(apiResult)
+        return JSON.parse(jsonResult)
+    }
+
+    map(apiResult){
+        const jsonResult = this.convert2json(apiResult);
+        const mappedResult = this.jsonConverter.map(jsonResult)
+        return mappedResult
+    }
 
 }
 
